adb911cc53d7a6629261d0d62054378b8e96ce8f
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2023-10-10 21:55:42 -0300

    Adicionando arquivos iniciais

---
+++ src/main/java/br/com/fabio/TestResource.java
@@ -0,0 +1,16 @@
+package br.com.fabio;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/ola")
+public class TestResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Olá povo!!!";
+    }
+}
